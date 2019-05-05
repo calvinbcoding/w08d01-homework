ac3aa7baaeda3a7ff646b2a7a49c083eea60edac
Calvin Bruch <calvinbcoding@gmail.com>
2019-05-05 15:11:11 -0600

    serviceworker not working

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import register as serviceWorker from './registerServiceWorker';
+import registerserviceWorker from './registerServiceWorker';
  //import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
