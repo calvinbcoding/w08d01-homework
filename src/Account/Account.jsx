46bb7315b91d86a14c9b99ffb83f0ab0e614c85c
Calvin Bruch <calvinbcoding@gmail.com>
2019-05-06 08:56:48 -0600

    "got deposit working"
    Signed-off-by: Calvin Bruch <calvinbcoding@gmail.com>

--- src/Account/Account.jsx
+++ src/Account/Account.jsx
@@ -5,40 +5,38 @@ class Account extends Component {
   constructor(props){
     super(props);
     this.state = {
-      blance: 0
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  
-  }
-  updateBalance = () => {
-    const balance = this.state.balance;
-    this.setState({
-      balance: event.target.balance});
-  }
-
-  handleSubmit(event) {
-    alert('Your Balance was updated to: ' + this.state.balance);
-    event.preventDefault();
-  }
-    render()  {
-      return (
-        <form onSubmit={this.handleSubmit}>
-        </form>
-
-        <div className="account">
-          <h2>{this.props.name}</h2>
-          
-          <div name="balance">
-          <h3>{this.state.balance}</h3></div>
-          
-          <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox/>
-          <input type="button" value="Deposit" />
-          <input type="button" value="Withdraw" />
-        </div>
-      )
+      balance: 0
     }
-  }
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleDepositClick = (e) => {
+      e.preventDefault();
+      const amount = parseInt(this.inputBox.value);
+      console.log(amount);
+      this.setState({
+        balance: this.state.balance + amount
+      })
+    }
+      
+  render(){
+      let balanceClass = 'balance';
+      if(this.state.balance === 0){ 
+        balanceClass += ' zero';
+      }
+      return (
+        
+
+          <div className="account">
+            <h2>{this.props.name}</h2> 
+          <div className={balanceClass}>{this.state.balance}</div>
+           
+          <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input}/>
+          <input type="button" value="Deposit" onClick={this.handleDepositClick}/>
+          <input type="button" value="Withdraw" onClick={this.handleWithdrawClick}/></div>
+        )
+  
+    }
+  } 
 
 export default Account;
 
